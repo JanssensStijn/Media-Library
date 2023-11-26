4efd3d8289112152fb4b08025f78bde0b5421162
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2023-11-26 21:52:16 +0100

    added pictures of books and new pictures of movies

--- src/main/java/be/thomasmore/medialibrary/repositories/BookRepository.java
+++ src/main/java/be/thomasmore/medialibrary/repositories/BookRepository.java
@@ -10,11 +10,10 @@ import java.util.Optional;
 public interface BookRepository extends CrudRepository<Book, Integer> {
 
     @Query("SELECT b FROM Book b WHERE (?1 IS NULL OR b.id = ?1)" +
-            " AND (?2 IS NULL OR b.id = ?2)" +
-            " AND (?3 IS NULL OR b.title LIKE ?3)" +
-            " AND (?4 IS NULL OR b.author LIKE ?4)"+
-            " AND (?5 IS NULL OR b.yearOfRelease = ?5)")
-    List<Book> findByFilter(Integer id, String title, String linkMoreInfo, Integer yearOfRelease);
+            " AND (?2 IS NULL OR b.title LIKE ?2)" +
+            " AND (?3 IS NULL OR b.author LIKE ?3)"+
+            " AND (?4 IS NULL OR b.yearOfRelease = ?4)")
+    List<Book> findByFilter(Integer id, String title, String author, Integer yearOfRelease);
     Optional<Book> findFirstByIdGreaterThanOrderByIdAsc(Integer id);
 
     Optional<Book> findFirstByOrderByIdAsc();

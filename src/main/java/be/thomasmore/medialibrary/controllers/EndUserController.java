9c9ff37ab55f890c6b83bb9310d93e2561423307
JanssensStijn <114300241+JanssensStijn@users.noreply.github.com>
2024-01-03 13:38:30 +0100

    add/remove movies or books to owned-list from any page

--- src/main/java/be/thomasmore/medialibrary/controllers/EndUserController.java
+++ src/main/java/be/thomasmore/medialibrary/controllers/EndUserController.java
@@ -22,28 +22,25 @@ public class EndUserController {
     private BookRepository bookRepository;
     @Autowired
     private EndUserRepository endUserRepository;
-    @ModelAttribute("movies")
-    public Iterable<Movie> findMovies(Principal principal){
-        List<Movie> movies = (List<Movie>) movieRepository.findAll();
-        EndUser userFromDb = endUserRepository.findByUsername(principal.getName());
-        return movies.stream().filter(movie -> movie.getEndUsersWishlist().contains(userFromDb)).toList();
-    }
-    @ModelAttribute("books")
-    public Iterable<Book> findBooks(Principal principal){
-        List<Book> books = (List<Book>) bookRepository.findAll();
-        EndUser userFromDb = endUserRepository.findByUsername(principal.getName());
-        return books.stream().filter(book -> book.getEndUsersWishlist().contains(userFromDb)).toList();
-    }
 
     @GetMapping("/wishlist")
-    public String MovieListWithFilter(Model model, Principal principal) {
+    public String Wishlist(Model model, Principal principal) {
         if(principal == null) return "redirect:/home";
-        model.addAttribute("username", principal.getName());
+
+        EndUser userFromDb = endUserRepository.findByUsername(principal.getName());
         model.addAttribute("currentUser", endUserRepository.findByUsername(principal.getName()));
+
+        List<Movie> movies = (List<Movie>) movieRepository.findAll();
+        model.addAttribute("movies", movies.stream().filter(movie -> movie.getEndUsersWishlist().contains(userFromDb)).toList());
+
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books",books.stream().filter(book -> book.getEndUsersWishlist().contains(userFromDb)).toList());
+
         return "enduser/wishlist";
     }
+
     @PostMapping("/wishlist/{type}/{id}")
-    public String movieEditPost(Model model,@PathVariable String type,  @PathVariable int id, Principal principal, HttpServletRequest request){
+    public String movieWishlistPost(Model model,@PathVariable String type,  @PathVariable int id, Principal principal, HttpServletRequest request){
         if(principal == null) return "redirect:/home";
 
         EndUser currentUser = endUserRepository.findByUsername(principal.getName());
@@ -58,6 +55,53 @@ public class EndUserController {
                 Book book = bookRepository.findById(id).get();
                 if (currentUser.getWishlistBooks().contains(book)) currentUser.getWishlistBooks().remove(book);
                 else currentUser.getWishlistBooks().add(book);
+                break;
+        }
+
+        endUserRepository.save(currentUser);
+
+        //go back to the page where item was added to the wishlist
+        String referer = request.getHeader("Referer");
+        if (referer != null && !referer.isEmpty()) {
+            // Redirect to the previous page
+            return "redirect:" + referer;
+        }
+        // Default redirect if referer is not available or empty
+        return "redirect:/home";
+    }
+
+    @GetMapping("/ownedlist")
+    public String Ownedlist(Model model, Principal principal) {
+        if(principal == null) return "redirect:/home";
+
+        EndUser userFromDb = endUserRepository.findByUsername(principal.getName());
+        model.addAttribute("currentUser", endUserRepository.findByUsername(principal.getName()));
+
+        List<Movie> movies = (List<Movie>) movieRepository.findAll();
+        model.addAttribute("movies", movies.stream().filter(movie -> movie.getEndUsersOwned().contains(userFromDb)).toList());
+
+        List<Book> books = (List<Book>) bookRepository.findAll();
+        model.addAttribute("books",books.stream().filter(book -> book.getEndUsersOwned().contains(userFromDb)).toList());
+
+        return "enduser/ownedlist";
+    }
+
+    @PostMapping("/ownedlist/{type}/{id}")
+    public String movieBoughtPost(Model model,@PathVariable String type,  @PathVariable int id, Principal principal, HttpServletRequest request){
+        if(principal == null) return "redirect:/home";
+
+        EndUser currentUser = endUserRepository.findByUsername(principal.getName());
+
+        switch (type) {
+            case "movie":
+                Movie movie = movieRepository.findById(id).get();
+                if (currentUser.getOwnedMovies().contains(movie)) currentUser.getOwnedMovies().remove(movie);
+                else currentUser.getOwnedMovies().add(movie);
+                break;
+            case "book":
+                Book book = bookRepository.findById(id).get();
+                if (currentUser.getOwnedBooks().contains(book)) currentUser.getOwnedBooks().remove(book);
+                else currentUser.getOwnedBooks().add(book);
                 break;
         }
 
